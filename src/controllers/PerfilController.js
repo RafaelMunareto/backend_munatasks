53acbeb0cef1a1f2a24dd6a4e688ffa5f171b16e
Munareto <rafael.munareto@caixa.gov.br>
2022-03-01 21:34:05 -0300

    ok

--- src/controllers/PerfilController.js
+++ src/controllers/PerfilController.js
@@ -18,7 +18,7 @@ class PerfilController {
     const { filename } = req.file;
     const { idStaff, name, nameTime, manager } = req.body;
     const schema = Yup.object().shape({
-      idStaff: Yup.array().required(),
+      // idStaff: Yup.array().required(),
       name: Yup.string().min(3).required(),
       nameTime: Yup.string().min(3).required(),
       manager: Yup.boolean().required(),
