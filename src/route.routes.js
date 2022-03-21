fab6a504e45d45d3378c82d17e12662c103038a2
Rafael Munareto <rafael.munareto@icloud.com>
2022-03-21 16:00:31 -0300

    ok

--- src/route.routes.js
+++ src/route.routes.js
@@ -33,6 +33,7 @@ routes.post('/usuarios', UserController.store);
 routes.get('/perfil/:id', checkId, PerfilController.show);
 routes.get('/perfil', PerfilController.index);
 
+routes.use(authMiddleware);
 routes.get('/usuarios', UserController.index);
 routes.get('/usuarios/:id', checkId, UserController.show);
 routes.put(
