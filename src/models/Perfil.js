f49fbe216a8369334a7f020f3f860ada39e69d98
Rafael Munareto <rafael.munareto@icloud.com>
2022-03-23 14:47:31 -0300

    ok

--- src/models/Perfil.js
+++ src/models/Perfil.js
@@ -1,18 +1,11 @@
 const mongo = require('mongoose');
 
-const PerfilSchema = new mongo.Schema(
-  {
-    idStaff: [{ type: mongo.Schema.Types.ObjectId, ref: 'Perfil' }],
-    manager: Boolean,
-    name: { type: mongo.Schema.Types.ObjectId, ref: 'User' },
-    nameTime: String,
-    urlImage: String,
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
+const PerfilSchema = new mongo.Schema({
+  idStaff: [{ type: mongo.Schema.Types.ObjectId, ref: 'Perfil' }],
+  manager: Boolean,
+  name: { type: mongo.Schema.Types.ObjectId, ref: 'User' },
+  nameTime: String,
+  urlImage: String,
+});
 
 module.exports = mongo.model('Perfil', PerfilSchema);
