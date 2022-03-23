5f7acda967a8b9ac05f14f4ebc784328486af14f
Rafael Munareto <rafael.munareto@icloud.com>
2022-03-23 10:52:06 -0300

    ok

--- src/models/Perfil.js
+++ src/models/Perfil.js
@@ -1,22 +1,11 @@
 const mongo = require('mongoose');
 
-const PerfilSchema = new mongo.Schema(
-  {
-    idStaff: [{ type: mongo.Schema.Types.ObjectId, ref: 'Perfil' }],
-    manager: Boolean,
-    name: { type: mongo.Schema.Types.ObjectId, ref: 'User' },
-    nameTime: String,
-    urlImage: String,
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
-
-PerfilSchema.virtual('perfilUrlImage').get(function () {
-  return `https://munatasks.herokuapp.com/files/${this.urlImage}`;
+const PerfilSchema = new mongo.Schema({
+  idStaff: [{ type: mongo.Schema.Types.ObjectId, ref: 'Perfil' }],
+  manager: Boolean,
+  name: { type: mongo.Schema.Types.ObjectId, ref: 'User' },
+  nameTime: String,
+  urlImage: String,
 });
 
 module.exports = mongo.model('Perfil', PerfilSchema);
